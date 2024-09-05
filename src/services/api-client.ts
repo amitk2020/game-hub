06153b568f76322ceb305cd307dc482a14af061b
Amit <amit.k03377@gmail.com>
2024-09-06 09:25:00 +1200

    secured the api key to push

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "70bc5fe215f84d6f880455c4eeccafde"
+    key: import.meta.env.VITE_API_KEY
   }
 });
